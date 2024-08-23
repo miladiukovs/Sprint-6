cb6659504ad64bd476f51708bf56b874ed8865e5
Kamila Adyukova <kamila.adyukova@yandex.ru>
2024-08-23 23:26:24 +0300

    add jacoco report

---
+++ src/test/java/ParametrizedLionTest.java
@@ -0,0 +1,44 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+@RunWith(Parameterized.class)
+public class ParametrizedLionTest {
+
+    private final String sexTest;
+    private final boolean maneTest;
+
+    public ParametrizedLionTest(String sexTest, boolean maneTest) {
+        this.sexTest = sexTest;
+        this.maneTest = maneTest;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] lionParams() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
+
+    @Before
+    public void setUp() {
+        Feline feline = Mockito.mock(Feline.class);
+    }
+
+    @Mock
+    Feline feline;
+
+
+    @Test
+    public void sexTest() throws Exception {
+        Lion lion = new Lion(sexTest); // Передаем мок в конструктор
+        Assert.assertEquals(maneTest, lion.doesHaveMane());
+    }
+}
